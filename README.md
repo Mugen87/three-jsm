658eb5b6a09f885c6a582829b7793bd876fab97b
Mugen87 <michael.herzog@human-interactive.org>
2021-02-24 18:27:49 +0100

    Upgrade dev dependencies.

--- README.md
+++ README.md
@@ -3,11 +3,4 @@ Simple project example using the ES6 modules of three.js.
 
 ---
 
-Include the resulting build file into a HTML page and run the following code to start the app.
-
-    const app = new MYAPP.App();
-    app.init();
-
-Demo: https://jsfiddle.net/r8fw2m4c/
-
-You can also start a local server by using `npm start` and then open http://localhost:5000/app/.
+You can start a local server by using `npm start` and then open http://localhost:5000/app/ for testing.
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import {
+	BoxBufferGeometry,
+	Mesh,
+	MeshBasicMaterial,
+	PerspectiveCamera,
+	Scene,
+	WebGLRenderer
+} from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+let camera, scene, renderer;
+
+class App {
+
+	init() {
+
+		camera = new PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+		camera.position.z = 400;
+
+		scene = new Scene();
+
+		const geometry = new BoxBufferGeometry( 200, 200, 200 );
+		const material = new MeshBasicMaterial();
+
+		const mesh = new Mesh( geometry, material );
+		scene.add( mesh );
+
+		renderer = new WebGLRenderer( { antialias: true } );
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		document.body.appendChild( renderer.domElement );
+
+		window.addEventListener( 'resize', onWindowResize, false );
+
+		const controls = new OrbitControls( camera, renderer.domElement );
+
+		animate();
+
+	}
+
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+
+}
+
+export default App;
