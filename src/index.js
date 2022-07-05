08e4ffe1acecdb122091cee40ba1da99d744da53
TomiRenaldi <tomirenaldi2@gmail.com>
2022-07-05 21:50:50 +0700

    :sparkles: Add box size

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ camera.position.z = 3
 scene.add(camera)
 
 // Test
-const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
+const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(2, 2, 2), new THREE.MeshNormalMaterial())
 scene.add(cube)
 
 // Renderer
