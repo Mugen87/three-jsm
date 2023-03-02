7f959fca6affcbaf0a2df0bb95cdc3548c1dce7c
Mugen87 <michael.herzog@human-interactive.org>
2023-03-02 10:04:11 +0100

    Updated dependencies.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 	WebGLRenderer
 } from 'three';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 let camera, scene, renderer;
 
