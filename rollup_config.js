75b703b7be44063b08a0a8c286024b43ac0df4bb
Mugen87 <michael.herzog@human-interactive.org>
2024-02-02 12:45:33 +0100

    Update dependencies.

--- rollup_config.js
+++ rollup_config.js
@@ -1,5 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve'; // locate and bundle dependencies in node_modules (mandatory)
-import { terser } from "rollup-plugin-terser"; // code minification (optional)
+import terser from '@rollup/plugin-terser'; // code minification (optional)
 
 export default {
 	input: 'src/main.js',
